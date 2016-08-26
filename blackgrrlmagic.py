1d58fde03958cfdd8171a9a7a78b84430e8cc8f9
Selina Musuta <selina@codeforprogress.org>
2016-08-26 13:40:36 -0400

    broke up file into 3 functions: sleep, save to csv, and fetch potential followers

--- blackgrrlmagic.py
+++ blackgrrlmagic.py
@@ -1,2 +1,88 @@
-# Build my first twitterbot with Python
-# This bot will retweet tweets that hashtag #blackgirlmagic
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# by Selina Musuta - @pumzi_code
+# Questions for Selina:  What are the parameters?
+# .gitignore should include credentials.py (contains my keys and tokens that i do not want to share publicly)
+
+# Notes: Add StreamListener object to monitor tweets in real time and catches them.
+# Import credentials as a module
+
+# Import Tweepy, time
+import tweepy, time
+
+# Import csv
+import csv
+
+# Import sys for use of sys.stdout (system specific parameters and functions)
+import sys
+
+# Configure Twitter API Throttle to every 15 seconds to avoid the fail whale
+# https://dev.twitter.com/rest/reference/get/users/lookup
+# API documentation:  Only 180 requests allowed per 15 minute interval
+api_sleep = 15 
+
+# up to 100 users fetched per request
+api_user_page_size = 100
+
+# Consumer key, consumer secret, access token, access secret allows access to the twitter API
+cons_key =  " "
+cons_secret =   " "
+access_token =  " "
+access_token_secret = " "
+
+# Create an authorization variable that the OAuthHandler class passes the consumer key and consumer secret
+auth = tweepy.OAuthHandler(cons_key, cons_secret)
+
+# The auth variable has an attribute that passes the access token and the secret access token
+auth.set_access_token(access_token, access_token_secret)
+
+# Setting the API with authorization keys
+api = tweepy.API(auth)
+
+# Setting an empty list called Users
+users = []
+
+# Count will start as 1
+count = 1
+
+# Write a function that sets sleep limit
+def throttleapi():
+    time.sleep(api_sleep)
+
+# In this example, call throttleapi() function that was set time.sleep(15)
+def limit_handled(cursor):
+    while True:
+        try:
+            yield cursor.next()
+        except tweepy.RateLimitError:
+            throttleapi()
+
+# Write a function that uses the blackgirlmagic query to fetch users name, the time/date they created the post and print out 100 unique user results. Later create a function to save user
+
+def fetch_users(followers):
+    for tweet in limit_handled(tweepy.Cursor(api.search, q=('blackgirlmagic')).items(api_user_page_size)):
+        print "Name:", tweet.author.name.encode('utf8')
+        print "Tweet created:", tweet.created_at
+        user = tweet.author.screen_name.encode('utf8')
+        print user
+        print "//////////////////"
+        if not users.__contains__(user):
+            users.append(user)
+            count += 1
+            print count
+    print '====================List of unique users in total' + str(len(users))
+    for u in users:
+        print u
+
+# Future:  Write a function that will write followers meta-data and encode the output
+def save_record(result):
+    # Create the csv row, delimited by commas, text-qualified with double quotes
+    # line continuations for readability (backslashes)
+    record = '"'+ \
+        u.screen_name+'", "'+ \
+        u.name+'", "'+ \
+        u.location+'", "'+ \
+        u.description \
+        +'"\n'
+    # Encoding the output to avoid defaulting 'ascii' which generated an error upon output redirection
+    sys.stdout.write(record.encode('utf-8'))
